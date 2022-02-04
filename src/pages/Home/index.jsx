e1078150c3c911dd675b79c36f6b22f7dc4760b2
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-02-04 14:22:11 -0300

    home removi as animações

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,38 +1,27 @@
 import './home.css'
 import foto from '../../assets/Foto/lucas.jpg'
 
-import { Animated } from "react-animated-css";
-
 export default function Home() {
   return (
-    <div className="box">
+    <div className="container">
+      <h1>Home</h1>
 
-      <div className='container home_flex'>
-
-        <div className='boxTexto'>
-          <Animated animationInDelay={200} animationIn="fadeInLeft" animationOut="fadeOut" isVisible={true}>
+      <div className="box">
+        <div className='home_flex'>
+        
+          <div className='boxTexto'>
             <p>Oi </p>
-          </Animated>
-          <Animated animationInDelay={200} animationIn="fadeInRight" animationOut="fadeOut" isVisible={true}>
             <p>Meu nome é </p>
-          </Animated>
-          <Animated animationInDelay={500} animationIn="fadeInDown" animationOut="fadeOut" isVisible={true}>
             <p className='corNone'>Lucas </p>
-          </Animated>
-          <Animated animationInDelay={200} animationIn="fadeInUp" animationOut="fadeOut" isVisible={true}>
             <p>Desenvolvedor </p>
-          </Animated>
-          <Animated animationInDelay={200} animationIn="fadeInLeft" animationOut="fadeOut" isVisible={true}>
             <p>FullStack </p>
-          </Animated>
-        </div>
+          </div>
 
-        <div className='boxFoto'>
-          <Animated animationInDelay={500} animationIn="fadeIn" animationOut="fadeOut" isVisible={true}>
+          <div className='boxFoto'>
             <img src={foto} alt="foto" />
-          </Animated>
-        </div>
+          </div>
 
+        </div>
       </div>
     </div>
   )
