66de3488292fcf2e046bd15fe4004f97497fd322
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-02-05 16:18:15 -0300

    footer completo

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,12 +1,30 @@
 import './footer.css'
 
+import { BsLinkedin, BsGithub } from "react-icons/bs";
+
 export default function index() {
+  const urlLinkedin = 'https://www.linkedin.com/in/lucas-fernando-1a75511a0/'
+  const urlGitHub = 'https://github.com/DigitalHDR'
+
   return (
     <nav>
-    <div className="container nav_footer">
-      <h1>Footer</h1>
-    </div>
-  </nav>
+      <div className="container nav_footer">
+        <div className='footerBaixo'>
+
+          <div><p className='nome'>Lucas</p></div>
+
+          <div><p>© Copyright 2021</p></div>
+
+          <div>
+            <ul>
+              <li><a rel="noreferrer noopener" href={urlLinkedin}><BsLinkedin id='BsLinkedin' /></a></li>
+              <li><a rel="noreferrer noopener" href={urlGitHub}><BsGithub id='BsGithub' /></a></li>
+            </ul>
+          </div>
+
+        </div>
+      </div>
+    </nav>
   )
 }
 
