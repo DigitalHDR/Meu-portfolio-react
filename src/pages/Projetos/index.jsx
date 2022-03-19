1fbe12c2933b51791907ac569f50f9089620adab
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-03-19 15:35:01 -0300

    filtro de pesquisa add

--- src/pages/Projetos/index.jsx
+++ src/pages/Projetos/index.jsx
@@ -1,38 +1,43 @@
 import React, { useState, useEffect } from 'react'
 import './projetos.css'
 import Card from '../../components/Card'
-import data from '../../data/data'
+import dataGeral from '../../data/data'
 
 export default function Projetos() {
-  const [dados, setDados] = useState([])
-  
-  const filtrarLista = (filtro) => {
-    const filtrado = data.especificacao.filter((resp) => resp.type === filtro)
-    return setDados(filtrado)
+  const [data, setData] = useState([])
+  const [filter, setFilter] = useState(data)
+
+  useEffect(() => {
+    const getProdutos = () => {
+      const response = dataGeral.especificacao
+      setFilter(response)
+      setData(response)
+    }
+    getProdutos()
+  }, [])
+
+  const filterProduto = (categoria) => {
+    const resultadoFilter = data.filter((resp) => resp.type === categoria)
+    return setFilter(resultadoFilter)
   }
 
   return (
     <div className='container top' id='projetos_link'> {/* id apenas para página única /> */}
       <h1>Lista de Projetos</h1>
       <br />
-
       <div className='FiltroBox'>
-
         <h2>Filtro de busca</h2>
-
         <div className='BoxButton'>
           {/* <Button title="JavaScript">JavaScript</Button> */}
-          <h1 onClick={() => filtrarLista('html-css-javascrip')}>Html Css Javascrip</h1>
-          <h1 onClick={() => filtrarLista('react')}>React</h1>
-          <h1 onClick={() => filtrarLista('react-native')}>React-Native</h1>
-          <h1 onClick={() => setDados(data.especificacao)}>Todos</h1>
+          <h1 onClick={() => filterProduto('html-css-javascrip')}>Html Css Javascrip</h1>
+          <h1 onClick={() => filterProduto('react')}>React</h1>
+          <h1 onClick={() => filterProduto('react-native')}>React-Native</h1>
+          <h1 onClick={() => setFilter(data)}>Todos</h1>
         </div>
-
       </div>
-
       <br />
       <div className='projeto_flex'>
-        {dados.map((item, index) => {
+        {filter.map((item, index) => {
           return (
             <div key={index}>
               <div className='projetos_card'>
