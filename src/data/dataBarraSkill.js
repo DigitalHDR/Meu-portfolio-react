9f15e7fecce346456b9855595fd5a5a6a9ea9089
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-05-11 13:59:20 -0300

    nova skill styles

--- src/data/dataBarraSkill.js
+++ src/data/dataBarraSkill.js
@@ -4,137 +4,135 @@ const dataBarraSkill = {
       id: 1,
       done: 95,
       title: 'HTML',
-      classSet: 'html'
+      img: 'https://i.postimg.cc/y8LhtSZ2/html.png',
     },
     {
       id: 2,
       done: 95,
       title: 'CSS',
-      classSet: 'css'
+      img: 'https://i.postimg.cc/vT5LNqrv/css.png',
     },
     {
       id: 3,
       done: 75,
       title: 'JAVASCRIPT',
-      classSet: 'javascript'
+      img: 'https://i.postimg.cc/4xfbW1mz/javascript.png',
     },
     {
       id: 4,
-      done: 75,
-      title: 'SASS',
-      classSet: 'sass'
+      done: 80,
+      title: 'REACT.JS',
+      img: 'https://i.postimg.cc/TYGrPcjZ/react.png',
     },
     {
       id: 5,
-      done: 90,
-      title: 'BOOTSTRAP',
-      classSet: 'bootstrap'
+      done: 80,
+      title: 'REACTNATIVE',
+      img: 'https://i.postimg.cc/3xxmXJc1/react-native.png',
     },
     {
       id: 6,
-      done: 80,
-      title: 'REACT.JS',
-      classSet: 'react'
+      done: 70,
+      title: 'REDUX',
+      img: 'https://i.postimg.cc/XvZ5P13y/redux.png',
     },
     {
       id: 7,
-      done: 70,
-      title: 'REDUX',
-      classSet: 'redux'
-    },
-    {
-      id: 8,
-      done: 80,
-      title: 'REACTNATIVE',
-      classSet: 'reactNative'
-    },
-    {
-      id: 9,
-      done: 50,
-      title: 'REACTFORM',
-      classSet: 'reactForm'
-    },
-    {
-      id: 10,
-      done: 40,
-      title: 'MONGODB',
-      classSet: 'mongodb'
-    },
-    {
-      id: 11,
-      done: 70,
-      title: 'STYLED-COMPONENTS',
-      classSet: 'styled'
-    }
-  ],
-  atributosDois: [
-    {
-      id: 1,
-      done: 75,
-      title: 'NEXT.JS',
-      classSet: 'next'
-    },
-    {
-      id: 2,
-      done: 35,
-      title: 'TYPESCRIPT',
-      classSet: 'typescript'
-    },
-    {
-      id: 3,
-      done: 40,
-      title: 'WEBPACK',
-      classSet: 'webpack'
-    },
-    {
-      id: 4,
-      done: 20,
-      title: 'FIREBASE',
-      classSet: 'firebase'
-    },
-    {
-      id: 5,
-      done: 85,
-      title: 'GIT & GITHUB',
-      classSet: 'git'
-    },
-    {
-      id: 6,
-      done: 60,
-      title: 'PHOTOSHOP',
-      classSet: 'photoshop'
+      done: 90,
+      title: 'BOOTSTRAP',
+      img: 'https://i.postimg.cc/TwzJQMDw/bootstrap.png',
     },
     {
       id: 8,
       done: 30,
       title: 'ILLUSTRATOR',
-      classSet: 'illustrator'
+      img: 'https://i.postimg.cc/YqfzNPqr/illustrator.png',
     },
     {
       id: 9,
-      done: 20,
-      title: 'FORMIK',
-      classSet: 'formik'
+      done: 90,
+      title: 'FIGMA',
+      img: 'https://i.postimg.cc/pThBDXy4/figma.png',
     },
     {
       id: 10,
-      done: 60,
-      title: 'NODE',
-      classSet: 'node'
+      done: 40,
+      title: 'MONGODB',
+      img: 'https://i.postimg.cc/rmrSDnPL/mongodb.png',
     },
     {
       id: 11,
       done: 20,
       title: 'MYSQL',
-      classSet: 'mySQL'
+      img: 'https://i.postimg.cc/9fSdF51p/mysql.png',
     },
     {
       id: 12,
-      done: 90,
-      title: 'FIGMA',
-      classSet: 'figma'
-    }
-  ]
-}
+      done: 75,
+      title: 'NEXT.JS',
+      img: 'https://i.postimg.cc/C5pbkG3n/next.png',
+    },
+    {
+      id: 13,
+      done: 60,
+      title: 'NODE',
+      img: 'https://i.postimg.cc/J03Zqy86/node.png',
+    },
+    {
+      id: 14,
+      done: 60,
+      title: 'PHOTOSHOP',
+      img: 'https://i.postimg.cc/598B1rzB/photoshop.png',
+    },
+    {
+      id: 15,
+      done: 20,
+      title: 'FIREBASE',
+      img: 'https://i.postimg.cc/fWdKyLG0/firebase.png',
+    },
+    {
+      id: 16,
+      done: 50,
+      title: 'REACTFORM',
+      img: 'https://i.postimg.cc/xTRLNbtq/react-hook-form.png',
+    },
+    {
+      id: 17,
+      done: 20,
+      title: 'FORMIK',
+      img: 'https://i.postimg.cc/28fxZYY9/formik.png',
+    },
+    {
+      id: 18,
+      done: 85,
+      title: 'GIT & GITHUB',
+      img: 'https://i.postimg.cc/6QTVX5Vq/git-github.png',
+    },
+    {
+      id: 19,
+      done: 75,
+      title: 'SASS',
+      img: 'https://i.postimg.cc/6pbRYxsX/sass.png',
+    },
+    {
+      id: 20,
+      done: 70,
+      title: 'STYLED-COMPONENTS',
+      img: 'https://i.postimg.cc/85M6pF7p/styled-componend.png',
+    },
+    {
+      id: 21,
+      done: 35,
+      title: 'TYPESCRIPT',
+      img: 'https://i.postimg.cc/7Lpzr1Yt/typescript.png',
+    },
+    {
+      id: 22,
+      done: 40,
+      title: 'WEBPACK',
+      img: 'https://i.postimg.cc/902qrW9G/webpack.png',
+    },
+  ],
+};
 
-export default dataBarraSkill
+export default dataBarraSkill;
